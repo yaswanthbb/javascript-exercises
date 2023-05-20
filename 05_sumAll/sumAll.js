f4b847f9f656f87e5f67c30e951986e47fd7eb94
Yaswanth <byaswanth2003@gmail.com>
2023-05-20 23:50:21 +0530

    Complete Exercise 05 [Ticket: 05]

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    let sum = 0;
+    if (
+        typeof start === "number" &&
+        typeof end === "number" &&
+        start > 0 &&
+        end > 0
+    ) {
+        if (start < end) {
+            for (let x = start; x <= end; x++) {
+                sum += x;
+            }
+        } else {
+            for (let x = end; x <= start; x++) {
+                sum += x;
+            }
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
