5d87eb98d0769b67ffbb73efa17ee0892c67806c
Yaswanth <byaswanth2003@gmail.com>
2023-05-21 11:04:04 +0530

    Complete Exercise 07 [Ticket: 07]

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const conversion = (temp - 32) * (5 / 9)
+  if(Number.isInteger(conversion)){
+    return conversion
+  }
+  else{
+    return +conversion.toFixed(1)
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const conversion = (temp * (9 / 5) + 32)
+  if(Number.isInteger(conversion)){
+    return conversion
+  }
+  else{
+    return +conversion.toFixed(1)
+  }                                                                   
 };
 
 // Do not edit below this line
