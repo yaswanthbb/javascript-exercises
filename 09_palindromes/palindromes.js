0bd1bbece63df0b5b1920fcc3fe4f621de4e9ef8
Yaswanth <byaswanth2003@gmail.com>
2023-06-20 12:50:49 +0530

    Complete Excercise 09 [Ticket: 09]

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z]/g,'')
+    let reverse = word.split("").reverse().join('')
 
+    return word === reverse
 };
-
 // Do not edit below this line
 module.exports = palindromes;
