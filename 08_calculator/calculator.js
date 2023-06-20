93a0160ede5b8fa2e4bf378032c0423fbc19bd73
Yaswanth <byaswanth2003@gmail.com>
2023-06-20 12:11:16 +0530

    Complete Excercise 08 [Ticket: 08]

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let summ = 0
+  arr.forEach(element => summ+=element);
+  return summ
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multi  = 1
+  arr.forEach(element=> multi*=element)
+  console.log(multi)
+  return multi
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1
+  for (let x = 1; x <= num; x++) {
+    fact = fact * x
+  }
+  return fact
 };
 
 // Do not edit below this line
